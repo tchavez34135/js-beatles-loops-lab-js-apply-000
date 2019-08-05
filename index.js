438ac5d36678cc6d6c045b237c7c109036ef1482
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 04:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,3 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return newArr;
 }
-function iLoveTheBeatles(x) {
-  var array = [];
-  do {
-    x = array.push("I love the Beatles!");
-    x++;
-  } while (x < 15); {
-    
-  }
-  return array;
-}
