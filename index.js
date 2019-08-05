626ce24eb0416c88a25a16a946625a2fa3d7ddcc
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 04:04:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const musicians = ["John", "Paul", "George", "Ringo"];
+const musicians = ["John", "Paul", "George", "Ringo"];
 
 const instruments = ["guitar", "guitar", "bass", "drums"];
 
@@ -12,7 +12,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 theBeatlesPlay(musicians, instruments);
 
-/*const facts = [
+const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
