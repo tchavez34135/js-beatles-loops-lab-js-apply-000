abf321b1b52a5bc217d14d2cd692c98cd8dca5ba
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 04:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
 var musicianString;
 var musicianInstrument=[];
 for(var i=0; i<musicians.length; i++){
-musicianString= musicians[i] + "plays" + instruments[i];
+musicianString= musicians[i] + " plays " + instruments[i];
 musicianInstrument[i]=musicianString;
 }
 return musicianInstrument;
