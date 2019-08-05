9246ce6edfce2dac153f696767f01144572baa3f
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 04:10:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-\var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const musicians = ["John", "Paul", "George", "Ringo"];
 
-
-theBeatlesPlay(musicians, instruments); 
+const instruments = ["guitar", "guitar", "bass", "drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  var newArr = []; 
-  for(var i =0; i < musicians.length; i++) {
-    newArr.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return newArr;
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
+  return array;
 }
+
+theBeatlesPlay(musicians, instruments);
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}*/
+
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
+
+//iLoveTheBeatles(12);
